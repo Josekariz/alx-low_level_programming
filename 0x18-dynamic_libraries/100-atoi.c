7ce09ea9b60ec06c9af3f71e8a06bfc3b9592155
Joseph <sejokarizz@gmail.com>
2023-06-19 21:17:25 +0300

    create atoi file

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+		else
+			break;
+	}
+
+	return (sign * result);
+}
